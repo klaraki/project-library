645e9364bfb1ca7acfdce6cd1fea5f9c240bcc44
klara kindlund <>
2023-10-09 19:33:54 +0200

    added code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # project-library
+
+# Project Name
+I have done a website for "my favorite recipes". 
+There are one filter function where the user can filter the recipes by country. There are also one sorting-function where the user can sort the recipes based on estimated cooking time. 
+
+## The problem
+First, all recipes in the object are displayed by a for each-loop. The loop goes through each recipe in the object, creating a div that displays the header, image, and estimated cooking time.
+The filter-function is made with the filter()-method, creating a temporary arrow that contains only the recipes that have a 'cuisineType' that matches the selected value from the dropdown. 
+The sorting-function is made with the sorting()-method, with one ascending sorting and one descending, based on the user's selected value. 
+
+I tried to break the project down to smaller functions, focusing on what I wanted the outcome to be like. For example, when creating the filter-function, I had a process in my head describing what value I wanted to use, how I could get the references (cuisineType), and if I could reuse the loadRecipes-function that I had already created to display all the recipes. 
+
+If I had more time I would've looked at the stretch goals this week and perhaps try to make a search bar, that would be nice to know how-to. 
+I'd also put more time on the design, but considering that this is a JS course I didn't focus on this.  
+
+
+
+## View it live
--- script.js
+++ script.js
@@ -2,193 +2,11 @@
 If you choose to create your own arrays with elements, just make sure that some
 of the properties make sense to filter on, and some to sort on.*/
 
-const books = [
-  {
-    title: 'The Great Gatsby',
-    author: 'F. Scott Fitzgerald',
-    year: 1925,
-    genre: 'Fiction',
-    rating: 4.2,
-    description:
-      'A classic novel set in the Roaring Twenties, it explores themes of wealth, love, and the American Dream through the enigmatic Jay Gatsby.',
-    image: './books-images/the-great-gatsby.jpg'
-  },
-  {
-    title: 'To Kill a Mockingbird',
-    author: 'Harper Lee',
-    year: 1960,
-    genre: 'Fiction',
-    rating: 4.5,
-    description:
-      'Set in the American South, this novel tackles issues of racism and injustice through the eyes of young Scout Finch.',
-    image: './books-images/to-kill-a-mockingbird.jpg'
-  },
-  {
-    title: '1984',
-    author: 'George Orwell',
-    year: 1949,
-    genre: 'Science Fiction',
-    rating: 4.4,
-    description:
-      'A dystopian classic that explores totalitarianism and the consequences of a surveillance state in a bleak future.',
-    image: './books-images/1984.jpg'
-  },
-  {
-    title: 'Pride and Prejudice',
-    author: 'Jane Austen',
-    year: 1813,
-    genre: 'Fiction',
-    rating: 4.25,
-    description:
-      'A timeless romance novel that examines societal expectations and the misunderstandings that can arise from pride and prejudice.',
-    image: './books-images/pride-and-prejudice.jpg'
-  },
-  {
-    title: 'The Catcher in the Rye',
-    author: 'J.D. Salinger',
-    year: 1951,
-    genre: 'Fiction',
-    rating: 4,
-    description:
-      'Narrated by the teenage Holden Caulfield, the novel explores themes of alienation and the search for authenticity.',
-    image: './books-images/unknown.jpg'
-  },
-  {
-    title: 'The Hobbit',
-    author: 'J.R.R. Tolkien',
-    year: 1937,
-    genre: 'Fantasy',
-    rating: 4.6,
-    description:
-      'A fantasy adventure novel that follows Bilbo Baggins on a quest to help a group of dwarves reclaim their homeland from a dragon.',
-    image: './books-images/the-hobbit.jpg'
-  },
-  {
-    title: "Harry Potter and the Sorcerer's Stone",
-    author: 'J.K. Rowling',
-    year: 1997,
-    genre: 'Fantasy',
-    rating: 4.7,
-    description:
-      'The first book in the beloved Harry Potter series, it introduces readers to the magical world of Hogwarts and the young wizard Harry Potter.',
-    image: "./books-images/harry-potter-and-the-sorcerer'.jpg"
-  },
-  {
-    title: 'Moby-Dick',
-    author: 'Herman Melville',
-    year: 1851,
-    genre: 'Adventure',
-    rating: 4.1,
-    description:
-      'An epic tale of obsession, revenge, and the relentless pursuit of the great white whale, Moby Dick.',
-    image: './books-images/moby-dick.jpg'
-  },
-  {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    author: 'J.R.R. Tolkien',
-    year: 1954,
-    genre: 'Fantasy',
-    rating: 4.55,
-    description:
-      'The first volume of the epic fantasy trilogy follows Frodo Baggins and the Fellowship on their quest to destroy the One Ring.',
-    image: './books-images/the-lord-of-the-rings.jpg'
-  },
-  {
-    title: 'The Shining',
-    author: 'Stephen King',
-    year: 1977,
-    genre: 'Horror',
-    rating: 4.3,
-    description:
-      "A psychological horror novel that tells the story of the Torrance family's terrifying experiences at the haunted Overlook Hotel.",
-    image: './books-images/unknown.jpg'
-  },
-  {
-    title: 'The Chronicles of Narnia: The Lion, the Witch and the Wardrobe',
-    author: 'C.S. Lewis',
-    year: 1950,
-    genre: 'Fantasy',
-    rating: 4.15,
-    description:
-      'The first book in the Chronicles of Narnia series, it follows the adventures of children who discover the magical land of Narnia.',
-    image: './books-images/the-chronicles-of-narnia.jpg'
-  },
-  {
-    title: 'The Da Vinci Code',
-    author: 'Dan Brown',
-    year: 2003,
-    genre: 'Mystery',
-    rating: 3.8,
-    description:
-      'A gripping mystery thriller that follows Harvard symbologist Robert Langdon as he unravels the secrets of the Da Vinci Code.',
-    image: './books-images/unknown.jpg'
-  },
-  {
-    title: 'The Alchemist',
-    author: 'Paulo Coelho',
-    year: 1988,
-    genre: 'Fiction',
-    rating: 4.25,
-    description:
-      'A philosophical novel that tells the story of Santiago, a shepherd boy, on his quest to discover his personal legend.',
-    image: './books-images/unknown.jpg'
-  },
-  {
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    year: 2008,
-    genre: 'Science Fiction',
-    rating: 4.3,
-    description:
-      "In a dystopian future, Katniss Everdeen becomes a symbol of rebellion when she volunteers to take her sister's place in the brutal Hunger Games.",
-    image: './books-images/unknown.jpg'
-  },
-  {
-    title: 'The Girl with the Dragon Tattoo',
-    author: 'Stieg Larsson',
-    year: 2005,
-    genre: 'Mystery',
-    rating: 4.1,
-    description:
-      'A gripping mystery novel featuring investigative journalist Mikael Blomkvist and the enigmatic hacker Lisbeth Salander.',
-    image: './books-images/unknown.jpg'
-  },
-  {
-    title: 'The Road',
-    author: 'Cormac McCarthy',
-    year: 2006,
-    genre: 'Dystopian',
-    rating: 4,
-    description:
-      "Set in a post-apocalyptic world, it follows a father and son's harrowing journey to survive and find safety.",
-    image: './books-images/unknown.jpg'
-  },
-  {
-    title: "The Hitchhiker's Guide to the Galaxy",
-    author: 'Douglas Adams',
-    year: 1979,
-    genre: 'Science Fiction',
-    rating: 4.35,
-    description:
-      "A comedic science fiction series that follows the misadventures of Arthur Dent after Earth's destruction.",
-    image: './books-images/unknown.jpg'
-  },
-  {
-    title: 'The Giver',
-    author: 'Lois Lowry',
-    year: 1993,
-    genre: 'Dystopian',
-    rating: 4.12,
-    description:
-      'A dystopian novel set in a seemingly perfect society where young Jonas discovers the dark truth beneath the surface.',
-    image: './books-images/unknown.jpg'
-  }
-]
 
 const recipes = [
   {
     name: 'Individual vegetarian lasagnes',
-    cuisineType: ['italian'],
+    cuisineType: 'Italian',
     ingredients: [
       '1.2 kg cherry tomatoes',
       '5 sprigs of fresh thyme',
@@ -229,13 +47,13 @@ const recipes = [
       '1/4 teaspoon ground white pepper'
     ],
     source: 'Red Cook',
-    totalTime: null,
+    totalTime: 20,
     URL: 'http://redcook.net/2010/06/16/garlic-scape-an-off-menu-treat/',
     image: './recipe-images/vegetarian-stir-fried-garlic-s.jpg'
   },
   {
     name: 'Cheat’s cheesy focaccia',
-    cuisineType: ['Italian'],
+    cuisineType: 'Italian',
     ingredients: [
       '500g pack bread mix',
       '2 tbsp olive oil , plus a little extra for drizzling',
@@ -295,7 +113,7 @@ const recipes = [
   },
   {
     name: 'Baked Chicken',
-    cuisineType: ['american'],
+    cuisineType: 'american',
     ingredients: [
       '6 bone-in chicken breast halves, or 6 chicken thighs and wings, skin-on',
       '1/2 teaspoon coarse salt',
@@ -318,7 +136,7 @@ const recipes = [
   },
   {
     name: 'Burnt-Scallion Fish',
-    cuisineType: ['chinese'],
+    cuisineType: 'chinese',
     ingredients: [
       '2 bunches scallions',
       '8 tbsp. butter',
@@ -404,3 +222,99 @@ const recipes = [
     image: './recipe-images/grilled.jpg'
   }
 ]
+
+
+// Get references to HTML elements using their IDs. 
+const filterDropDown = document.getElementById("filterDropDown")
+const sortingDropDown = document.getElementById("sortingDropDown")
+const container = document.getElementById("container");
+
+
+// Function to load and display the list of recipes. 
+const loadRecipes = (recipeArray) => {
+    container.innerHTML = "";
+    
+    recipeArray.forEach((recipe) => {
+
+    container.innerHTML += `
+    <div class="card">
+    <p>${recipe.name}</p>
+    <img src=${recipe.image} art=${recipe.name}>
+    <p><b>Total time:</b> ${recipe.totalTime} min</p>
+      </div>
+`;
+})
+}
+
+//Function to filter recipes.
+const filterRecipes = () => {
+
+  // Get the selected value from the filter dropdown.
+ const value = filterDropDown.value;
+
+ if (value === "all") {
+   loadRecipes(recipes);
+
+  } else {
+    // Otherwise, filter recipes based on country and load the filtered list.
+    const filteredList = recipes.filter((recipe) => recipe.cuisineType === value);
+
+    loadRecipes(filteredList);
+  }
+};
+
+
+
+//Function to sort recipes.
+const sortRecipes = () => {
+
+  // Get the selected value from the sorting dropdown.
+ const sortingValue = sortingDropDown.value;
+
+
+ if (sortingValue === "Sort by time") {
+   loadRecipes(recipes);
+
+  } else if (sortingValue === "lowest") {
+    // Sort the recipes by lowest time and load the filtered list. 
+
+    const sortedByLowest = recipes.sort((recipeOne, recipeTwo) => (recipeOne.totalTime > recipeTwo.totalTime) ? 1 : (recipeOne.totalTime < recipeTwo.totalTime) ? -1 : 0);
+
+    loadRecipes(sortedByLowest);
+
+  } else if (sortingValue === "highest") {
+    // Sort the recipes by highest time and load the filtered list. 
+
+    const sortedByHighest = recipes.sort((recipeOne, recipeTwo) => (recipeOne.totalTime < recipeTwo.totalTime) ? 1 : (recipeOne.totalTime > recipeTwo.totalTime) ? -1 : 0);
+
+    loadRecipes(sortedByHighest);
+
+  } else {
+
+    loadRecipes(recipes);
+  }
+
+
+
+};
+
+
+// Apply the sorting when the user changes the sorting selection.
+sortingDropDown.addEventListener("change", sortRecipes);
+
+
+// Apply the filter when the user changes the dropdown selection.
+filterDropDown.addEventListener("change", filterRecipes);
+
+
+// Load the list of recipes when the page loads.
+loadRecipes(recipes);
+
+
+
+
+
+
+
+
+
